adf84461312c9215f96375661ddef2b919dfb6d5
Waifu Forever <itachiut1r4@gmail.com>
2022-09-14 21:15:22 -0300

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ['./src/**/*.{js,jsx,ts,tsx}'],
+    theme: {
+        extend: {
+            screens: {
+                '0xs': '1px',
+                xxs: '319px',
+                '2xxs': '360px',
+                xs: '450px',
+                '2xs': '555px',
+                '2md': '888px',
+                '2lg': '1124px',
+                sm: '640px',
+                md: '768px',
+                lg: '1024px',
+                xl: '1280px',
+                '2xl': '1536px',
+            },
+        },
+    },
+    plugins: [],
+};
