f25cce1a86b4cae24084e8ad5c6c9041fff55800
Waifu Forever <itachiut1r4@gmail.com>
2022-09-25 16:53:10 -0300

    refactor: select tools     save drawed elements;     change the mouse behaviour by clicking on the buttons inside the tool-menu;     fix continuous line problem;     fix line doesn't keep up with a high speed cursor;

--- src/components/ToolButton.jsx
+++ src/components/ToolButton.jsx
@@ -1,6 +1,9 @@
 const ToolButton = ({ icon: Icon, action }) => {
     return (
-        <div className="flex w-7 h-7 m-1 rounded-md bg-blue-100 hover:bg-gray-300  justify-center items-center" onClick={() => action}>
+        <div
+            className="flex w-7 h-7 m-1 rounded-md bg-blue-100 hover:bg-gray-300  justify-center items-center"
+            onClick={action}
+        >
             {Icon}
         </div>
     );
--- src/pages/DrawScreen.jsx
+++ src/pages/DrawScreen.jsx
@@ -1,14 +1,116 @@
+import { useEffect, useRef, useState } from 'react';
 import { FaPencilAlt } from 'react-icons/fa';
 import { GiStraightPipe } from 'react-icons/gi';
 import ToolButton from '../components/ToolButton';
 
-import { usePencil } from '../hooks/ToolHooks';
-
 const DrawScreen = () => {
+    const [elements, setElements] = useState([]);
+    const [elementType, setElementType] = useState('pencil');
     const onDraw = (ctx, point, prevPoint) => {
         drawLine(prevPoint, point, ctx, '#000000', 2);
     };
-    const { setCanvasRef, onCanvasMouseDown } = usePencil(onDraw);
+
+    const computePointInCanvas = (clientX, clientY, canvasRef) => {
+        if (!canvasRef.current) {
+            return null;
+        }
+        const boundingRect = canvasRef.current.getBoundingClientRect();
+
+        return {
+            x: clientX - boundingRect.left,
+            y: clientY - boundingRect.top,
+        };
+    };
+
+    const canvasRef = useRef(null);
+    const isDrawingRef = useRef(false);
+    const prevPointRef = useRef(null);
+
+    const mouseMoveListenerRef = useRef(null);
+    //const mouseDownListenerRef = useRef(null);
+    const mouseUpListenerRef = useRef(null);
+
+    const setCanvasRef = (ref) => {
+        canvasRef.current = ref;
+    };
+
+    const onCanvasMouseDown = (event) => {
+        isDrawingRef.current = true;
+        setElements((prevState) => [
+            ...prevState,
+            [
+                computePointInCanvas(event.clientX, event.clientY, canvasRef),
+                elementType,
+            ],
+        ]);
+    };
+
+    useEffect(() => {
+        const initMouseMoveListener = () => {
+            const listener = (e) => {
+                if (!isDrawingRef.current || !canvasRef.current) return;
+                switch (elementType) {
+                    case 'pencil':
+                        console.log('pencil');
+                        const point = computePointInCanvas(
+                            e.clientX,
+                            e.clientY,
+                            canvasRef
+                        );
+                        const head = elements.slice(0, elements.length - 1);
+                        const tail = elements[elements.length - 1];
+
+                        tail.push(point);
+                        head.push(tail);
+                        console.log({ tail });
+                        setElements(head);
+
+                        const ctx = canvasRef.current.getContext('2d');
+                        if (onDraw) onDraw(ctx, point, prevPointRef.current);
+                        prevPointRef.current = point;
+                        console.log({ elements });
+                        break;
+
+                    case 'line':
+                        console.log('line');
+                        break;
+
+                    default:
+                        break;
+                }
+            };
+            mouseMoveListenerRef.current = listener;
+            window.addEventListener('mousemove', listener);
+        };
+
+        const initMouseUpListener = () => {
+            const listener = () => {
+                isDrawingRef.current = false;
+                prevPointRef.current = null;
+            };
+            mouseUpListenerRef.current = listener;
+            window.addEventListener('mouseup', listener);
+        };
+
+        const removeListeners = () => {
+            if (mouseMoveListenerRef.current)
+                window.removeEventListener(
+                    'mousemove',
+                    mouseMoveListenerRef.current
+                );
+            if (mouseUpListenerRef.current)
+                window.removeEventListener(
+                    'mouseup',
+                    mouseUpListenerRef.current
+                );
+        };
+
+        initMouseMoveListener();
+        initMouseUpListener();
+        return () => {
+            removeListeners();
+        };
+    }, [onDraw, elements]);
 
     const drawLine = (start, end, ctx, colour, width) => {
         start = start ?? end;
@@ -28,8 +130,20 @@ const DrawScreen = () => {
     return (
         <div className="flex h-full w-full justify-center p-5 bg-skin-default">
             <div className="flex justify-center items-center ">
-                <ToolButton icon={<FaPencilAlt />} onClick={() => {}} />
-                <ToolButton icon={<GiStraightPipe />} onClick={() => {}} />
+                <ToolButton
+                    icon={<FaPencilAlt />}
+                    action={() => {
+                        setElementType('pencil');
+                        console.log(elementType);
+                    }}
+                />
+                <ToolButton
+                    icon={<GiStraightPipe />}
+                    action={() => {
+                        setElementType('line');
+                        console.log(elementType);
+                    }}
+                />
             </div>
             <div className="mx-4 overflow-y-auto">
                 <canvas
