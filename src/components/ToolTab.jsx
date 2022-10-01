8f677c73fb742c915eb9f819890f2774868a91a0
Waifu Forever <itachiut1r4@gmail.com>
2022-09-30 21:50:02 -0300

    feat: drag items     keeping the selected element saved using useState;     change the cursor when dragging elements;     correct mouse offset while dragging elements;     improve tooltab alignment;

--- src/components/ToolTab.jsx
+++ src/components/ToolTab.jsx
@@ -5,7 +5,7 @@ import { AiOutlineCaretDown, AiOutlineCaretUp } from 'react-icons/ai';
 const ToolTab = ({ title, tools }) => {
     const [isDisplayed, setIsDisplayed] = useState(true);
     return (
-        <div className="w-full">
+        <div className="w-32 flex flex-col justify-center items-center">
             <div
                 className={`flex items-center justify-between rounded w-32 bg-green-600 text-center px-3 py-1`}
                 onClick={() => setIsDisplayed(!isDisplayed)}
@@ -14,7 +14,7 @@ const ToolTab = ({ title, tools }) => {
                 {isDisplayed ? <AiOutlineCaretDown /> : <AiOutlineCaretUp />}
             </div>
             <div
-                className={`flex ${isDisplayed ? '' : 'hidden'} justify-center`}
+                className={`flex flex-wrap ${isDisplayed ? '' : 'hidden'} justify-center items-center`}
             >
                 {tools.map((Tool, index) => (
                     <div key={index}>{Tool}</div>
--- src/pages/DrawScreen.jsx
+++ src/pages/DrawScreen.jsx
@@ -3,6 +3,7 @@ import { v4 as uuid } from 'uuid';
 import { FaPencilAlt } from 'react-icons/fa';
 import { BiRectangle } from 'react-icons/bi';
 import { BsFillLayersFill } from 'react-icons/bs';
+import { FiMousePointer } from 'react-icons/fi';
 import { GiStraightPipe } from 'react-icons/gi';
 
 import ToolTab from '../components/ToolTab';
@@ -10,9 +11,52 @@ import ToolButton from '../components/ToolButton';
 import Layer from '../components/Layer';
 import ColourPicker from '../components/ColourPicker';
 
-const createElement = (startPoint, endPoint, elementType, width, colour) => {
-    let id = uuid();
+const createElement = (
+    startPoint,
+    endPoint,
+    elementType,
+    width,
+    colour,
+    id
+) => {
+    id = id ?? uuid();
     return { startPoint, endPoint, elementType, width, colour, id };
+};
+
+const isWithinElement = (x, y, element) => {
+    const { startPoint, endPoint, elementType } = element;
+
+    switch (elementType) {
+        case 'rectangle':
+            const minX = Math.min(startPoint.x, endPoint.x);
+            const maxX = Math.max(startPoint.x, endPoint.x);
+            const minY = Math.min(startPoint.y, endPoint.y);
+            const maxY = Math.max(startPoint.y, endPoint.y);
+
+            return x >= minX && x <= maxX && y >= minY && y <= maxY;
+
+        case 'straightLine':
+            const p = { x, y };
+            const offset =
+                distance(startPoint, endPoint) -
+                (distance(startPoint, p) + distance(endPoint, p));
+            return Math.abs(offset) < 1;
+
+        default:
+            const p2 = { x, y };
+            const offset2 =
+                distance(startPoint, endPoint) -
+                (distance(startPoint, p2) + distance(endPoint, p2));
+            return Math.abs(offset2) < 1;
+    }
+};
+
+const distance = (a, b) =>
+    Math.sqrt(Math.pow(a.x - b.x, 2) + Math.pow(a.y - b.y, 2));
+
+const getElementAtPosition = (x, y, elements) => {
+    console.log(elements.find((element) => isWithinElement(x, y, element)));
+    return elements.find((element) => isWithinElement(x, y, element));
 };
 
 const drawElement = (element, context) => {
@@ -48,6 +92,7 @@ const drawElement = (element, context) => {
 const DrawScreen = () => {
     const [elements, setElements] = useState([]);
     const [elementType, setElementType] = useState('rectangle');
+    const [selectedElement, setSelectedElement] = useState(null);
     const [isDrawing, setIsDrawing] = useState(false);
     const [displayCanvas, setDisplayCanvas] = useState(true);
     const [colour, setColour] = useState('#000000');
@@ -72,7 +117,6 @@ const DrawScreen = () => {
     };
 
     useLayoutEffect(() => {
-        console.log(elements);
         if (!drewElementsRef.current) {
             const ctx = canvasRef.current.getContext('2d');
             console.log(elements);
@@ -90,52 +134,108 @@ const DrawScreen = () => {
         }
     }, [elements]);
 
-    const handleMouseDown = (event) => {
-        setIsDrawing(true);
-
-        const point = computePointInCanvas(
-            event.clientX,
-            event.clientY,
-            canvasRef.current
+    const updateElement = (element) => {
+        const updatedElement = createElement(
+            element.startPoint,
+            element.endPoint,
+            element.elementType,
+            element.width,
+            element.colour,
+            element.id
         );
+        console.log(updatedElement);
+        const elementsCopy = [...elements];
 
-        const element = createElement(point, point, elementType, 2, colour);
+        elementsCopy[elementsCopy.findIndex((e) => e.id === element.id)] =
+            updatedElement;
+        setElements(elementsCopy);
+    };
 
-        setElements((prevState) => [...prevState, element]);
+    const handleMouseDown = (event) => {
+        const { clientX, clientY } = event;
+        setIsDrawing(true);
+        const point = computePointInCanvas(clientX, clientY, canvasRef.current);
+        if (elementType === 'select') {
+            const element = getElementAtPosition(point.x, point.y, elements);
+
+            if (!element) return;
+
+            const offsetX = point.x - element.startPoint.x;
+            const offsetY = point.y - element.startPoint.y;
+
+            setSelectedElement({
+                ...element,
+                offset: { x: offsetX, y: offsetY },
+            });
+        } else {
+            const element = createElement(point, point, elementType, 2, colour);
+
+            setElements((prevState) => [...prevState, element]);
+        }
     };
 
     const handleMouseMove = (event) => {
-        if (!isDrawing) return;
+        if (!isDrawing) {
+            event.target.style.cursor = 'default';
 
+            return;
+        }
+        const { clientX, clientY } = event;
         const index = elements.length - 1;
+        const point = computePointInCanvas(clientX, clientY, canvasRef.current);
 
-        const point = computePointInCanvas(
-            event.clientX,
-            event.clientY,
-            canvasRef.current
-        );
+        if (elementType === 'select') {
+            event.target.style.cursor = getElementAtPosition(
+                point.x,
+                point.y,
+                elements
+            )
+                ? 'move'
+                : 'default';
 
-        const updatedElement = createElement(
-            elements[index].startPoint,
-            point,
-            elementType,
-            2,
-            colour
-        );
+            if (!selectedElement) return;
+            const { startPoint, endPoint, id, offset } = selectedElement;
 
-        const elementsCopy = [...elements];
-        elementsCopy[index] = updatedElement;
-        setElements(elementsCopy);
+            const width = endPoint.x - startPoint.x;
+            const height = endPoint.y - startPoint.y;
+
+            const correctedPosition = {
+                x: point.x - offset.x,
+                y: point.y - offset.y,
+            };
+
+            updateElement({
+                startPoint: { x: correctedPosition.x, y: correctedPosition.y },
+                endPoint: {
+                    x: correctedPosition.x + width,
+                    y: correctedPosition.y + height,
+                },
+                elementType: selectedElement.elementType,
+                width: selectedElement.width,
+                colour: selectedElement.colour,
+                id,
+            });
+        } else {
+            updateElement({
+                startPoint: elements[index].startPoint,
+                endPoint: point,
+                elementType: elementType,
+                width: 2,
+                colour,
+                id: elements[index].id,
+            });
+        }
         drewElementsRef.current = false;
     };
 
     const handleMouseUp = (event) => {
+        setSelectedElement(null);
         setIsDrawing(false);
     };
 
     return (
         <div className="flex h-full w-full justify-center p-5 bg-skin-default">
-            <div className="flex flex-col items-center gap-3 ">
+            <div className="flex flex-col flex-wrap items-center gap-3 ">
                 <ToolTab
                     title={'Title'}
                     tools={[
@@ -158,6 +258,13 @@ const DrawScreen = () => {
                             selected={elementType === 'rectangle'}
                             action={() => {
                                 setElementType('rectangle');
+                            }}
+                        />,
+                        <ToolButton
+                            icon={<FiMousePointer />}
+                            selected={elementType === 'select'}
+                            action={() => {
+                                setElementType('select');
                             }}
                         />,
                     ]}
